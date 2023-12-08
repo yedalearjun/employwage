a1e0c45f7d9ba24dbb6091ffe3881b9af45e6f6d
ARJUN YEDALE <arjunyedale8748@gmail.com>
2023-12-08 13:35:42 +0530

    checked:present Absent

--- src/com/employee/Employee1.java
+++ src/com/employee/Employee1.java
@@ -2,6 +2,13 @@ package com.employee;
 
 public class Employee1 {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage");
+        System.out.println("Welcome to employee wage");
+        int employeeCheck = (int) Math.floor(Math.random()*10)%2;
+        System.out.println(employeeCheck);
+        if (employeeCheck == 1){
+            System.out.println("Employee is present");
+        }else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
