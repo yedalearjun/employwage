df794c3f64f51140e62ded177c43dff803f2d547
ARJUN YEDALE <arjunyedale8748@gmail.com>
2023-12-08 13:40:22 +0530

    Calculated:Daily Wage

--- src/com/employee/Employee1.java
+++ src/com/employee/Employee1.java
@@ -1,14 +1,21 @@
 package com.employee;
 
 public class Employee1 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to employee wage");
-        int employeeCheck = (int) Math.floor(Math.random()*10)%2;
-        System.out.println(employeeCheck);
-        if (employeeCheck == 1){
-            System.out.println("Employee is present");
-        }else {
-            System.out.println("Employee is Absent");
+
+        static final int WAGE_PER_HOUR = 20;
+        static final int FULL_DAY_HOUR = 8;
+        public static void main(String[] args) {
+            System.out.println("Welcome to employee wage`");
+            int employeeCheck = (int) Math.floor(Math.random()*10)%2;
+            System.out.println(employeeCheck);
+            int dailyWage = 0;
+            if (employeeCheck == 1){
+                dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                System.out.println("Employee is present");
+            }else {
+                System.out.println("Employee is Absent");
+            }
+            System.out.println(" Employee daily wage is "+dailyWage);
         }
     }
-}
+
