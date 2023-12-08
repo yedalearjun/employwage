356351c1eb74cdc87e92bb5c0963f4c950c217f4
ARJUN YEDALE <arjunyedale8748@gmail.com>
2023-12-08 13:58:06 +0530

    Calculated:Monthly Wage

--- src/com/employee/Employee1.java
+++ src/com/employee/Employee1.java
@@ -7,25 +7,39 @@ public class Employee1 {
     static final int PART_TIME_HOUR = 4;
     static final int IS_FULL_TIME = 2;
     static final int IS_PART_TIME = 1;
+    static final int WORKING_DAYS_PER_MONTH = 20;
+
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage");
-        int employeeCheck = (int) Math.floor(Math.random()*10)%3;
-        System.out.println(employeeCheck);
+        int totalWage = 0;
         int dailyWage = 0;
+        int dayCount = 1;
+        while (dayCount <=WORKING_DAYS_PER_MONTH  ) {
+            int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
+            System.out.println(employeeCheck);
 
-        switch (employeeCheck){
-            case IS_PART_TIME:
-                dailyWage =PART_TIME_HOUR * WAGE_PER_HOUR;
-                System.out.println("Employee is part time ");
-                break;
-            case IS_FULL_TIME:
-                dailyWage =FULL_DAY_HOUR * WAGE_PER_HOUR;
-                System.out.println("Employee is full time ");
-                break;
-            default:
-                System.out.println("Employee is Absent");
+            switch (employeeCheck) {
+                case IS_PART_TIME:
+                    dailyWage = PART_TIME_HOUR * WAGE_PER_HOUR;
+                    System.out.println("Employee is part time " +dailyWage);
+                    break;
+                case IS_FULL_TIME:
+                    dailyWage = FULL_DAY_HOUR * WAGE_PER_HOUR;
+                    System.out.println("Employee is full time " +dailyWage);
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    break;
+            }
+            totalWage= totalWage + dailyWage;
+            dayCount++;
         }
-        System.out.println("Employee daily wage is " + dailyWage);
+        System.out.println(" daily wage for day "+dayCount+" =>" + dailyWage);
+        System.out.println(" daily wage for month "+dayCount+" =>" + totalWage);
+
+
     }
 }
+
+
 
